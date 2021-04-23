2796e1e93f1aa00bea28533f42e5a51849e679c1
Jarrod <jarrod.athanassiou@gmail.com>
2021-04-23 15:51:56 +1000

    Minesweeper

---
+++ Assets/element.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elements : MonoBehaviour
+{
+    // Is this a mine?
+    public bool mine;
+
+    // Different Textures
+    public Sprite[] emptyTextures;
+    public Sprite mineTexture;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Randomly decide if it's a mine or not
+        mine = Random.value < 0.15;
+
+        int x = (int)transform.position.x;
+        int y = (int)transform.position.y;
+        Playfield.elements[x, y] = this;
+    }
+
+    public void loadTexture(int adjacentCount)
+    {
+        if (mine)
+            GetComponent<SpriteRenderer>().sprite = mineTexture;
+        else
+            GetComponent<SpriteRenderer>().sprite = emptyTextures[adjacentCount];
+    }
+
+    public bool isCovered()
+    {
+        return GetComponent<SpriteRenderer>().sprite.texture.name == "default";
+    }
+
+    void OnMouseUpAsButton()
+    {
+        // It's a mine
+        if (mine)
+        {
+            // uncover all mines
+            Playfield.uncoverMines();
+
+            // game over
+            print("you lose");
+        }
+        // It's not a mine
+        else
+        {
+            // show adjacent mine number
+            int x = (int)transform.position.x;
+            int y = (int)transform.position.y;
+            loadTexture(Playfield.adjacentMines(x, y));
+
+            // uncover area without mines
+            Playfield.FFuncover(x, y, new bool[Playfield.w, Playfield.h]);
+
+
+            // find out if the game was won now
+            if (Playfield.isFinished())
+                print("you win");
+        }
+    }
+}
